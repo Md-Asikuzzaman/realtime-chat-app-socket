8b3db8c8a05dd8b34b4714f816a9ea080fac9cd6
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-07-30 10:39:48 +0600

    feat: added new-message logic

--- app.js
+++ app.js
@@ -19,13 +19,67 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Socket server runing..." });
 });
 
+// Store user IDs and their associated socket IDs
+const userSockets = new Map();
+
 // Socket events
 io.on("connection", (socket) => {
   console.log(socket.id);
 
+  // register user who connected
+  socket.on("registerUser", (userId) => {
+    userSockets.set(userId, socket.id);
+
+    console.log(`User ${userId} registered with socket ID ${socket.id}`);
+  });
+
+  // send message to a specific user
+
+  // Handle sending a message to a specific user
   socket.on("newMessage", (message) => {
-    console.log(message);
-    io.emit("newServerMessage", message);
+    const targetSocketId = userSockets.get(message.receiverId);
+
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("newServerMessage", {
+        message,
+        receiverId: message.receiverId,
+        senderId: message.senderId,
+      });
+      console.log(`Private message sent to ${message.senderId}`);
+    } else {
+      console.log(`User ${message.senderId} not found`);
+    }
+  });
+
+  socket.on("newMessage", (message) => {
+    const targetSocketId = userSockets.get(message.receiverId);
+
+    if (targetSocketId) {
+      socket.emit("newServerMessage", {
+        message,
+        receiverId: message.receiverId,
+        senderId: message.senderId,
+      });
+      console.log(`Private message sent to ${message.senderId}`);
+    } else {
+      console.log(`User ${message.senderId} not found`);
+    }
+  });
+
+  //   socket.on("newMessage", (message) => {
+  //     socket.emit("newServerMessage", message);
+  //   });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    // Find and remove user from the map
+    for (let [userId, socketId] of userSockets.entries()) {
+      if (socketId === socket.id) {
+        userSockets.delete(userId);
+        console.log(`User ${userId} disconnected`);
+        break;
+      }
+    }
   });
 });
 

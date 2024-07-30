82a6ed40c5da70379e22249224b363b8ec4751be
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-07-30 12:47:29 +0600

    fix: change client url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mychatbd.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },

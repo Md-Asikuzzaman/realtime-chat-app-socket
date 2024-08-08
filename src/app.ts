1b121a434b59542a8b44471169b23bb6a8bfeec1
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-08-08 18:12:15 +0600

    fix: convert js to ts

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,47 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { Message, Typing } from "../types";
 
 const PORT = 8080 || process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    // origin: "http://localhost:3000",
+    // origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Socket server runing..." });
 });
 
 // ================================================================
 
 // Object to map user IDs to socket IDs
-const userSocketMap = new Map();
+const userSocketMap = new Map<string, string>();
 
 io.on("connection", (socket) => {
   // Handle user ID registration
-  socket.on("registerUser", (userId) => {
-    // console.log("registerUser:", userId);
+  socket.on("registerUser", (userId: string) => {
     userSocketMap.set(userId, socket.id);
+
     const userList = Array.from(userSocketMap.entries()).map(
       ([userId, socketId]) => ({ userId, socketId })
     );
+
     io.emit("updateUsers", userList);
   });
 
   // Handle new message and send to the specific receiver
-  socket.on("newMessage", (data) => {
+  socket.on("newMessage", (data: Message) => {
     const { receiverId } = data;
     const receiverSocketId = userSocketMap.get(receiverId);
 
@@ -52,7 +56,7 @@ io.on("connection", (socket) => {
   // Handle Friend Typing
 
   // Handle new message and send to the specific receiver
-  socket.on("isTyping", (data) => {
+  socket.on("isTyping", (data: Typing) => {
     const { friendId } = data;
     const receiverId = friendId;
 
